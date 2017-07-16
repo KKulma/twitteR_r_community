f9938b10f102a908e6b1b44802389455fa0c0779
Kasia <katarzyna.kulma@gmail.com>
2017-07-16 22:08:56 +0100

    working out the network graph

--- twitter_setup.R
+++ twitter_setup.R
@@ -58,10 +58,10 @@ library(rtweet)
 
 ### log in ####
 
-twitter_token <- create_token(
-  app = "R-play",
-  consumer_key = api_key,
-  consumer_secret = api_secret)
+#twitter_token <- create_token(
+#  app = "R-play",
+#  consumer_key = api_key,
+#  consumer_secret = api_secret)
 
 ### getting #rstats users ####
 ?search_users
@@ -72,16 +72,16 @@ r_users <- search_users("#rstats", n = 1000)
 #r_users_ii <- search_users("#rstats", n = 1000, page = page)
 #r_users <- c(unlist(r_users), unlist(r_users_ii))
 
-
-str(r_users)
-
+?str
+str(r_users, max.level = 1)
+head(r_users)
 
 
 library(stringr)
 library(dplyr)
 
 
-### only 527 users
+### only 553 users
 r_users %>% summarise(n_users = n_distinct(screen_name))
 
 r_users_names <- r_users$screen_name %>% unique()
@@ -92,26 +92,31 @@ sort(r_users_names)
 
 r_users_info <- lookup_users(r_users_names)
 str(r_users_info)
+str(r_users)
 
 
+##### r_users' frequency of #rstats ####
 
+
+r_users_tweets<-map(r_users$screen_name, userTimeline,100)
 
 
 
 ### popularity/activity score: 
 
-r_users_info %>% select(contains("count")) %>% names()
+r_users_info %>% select(dplyr::contains("count")) %>% head()
 
 ## you can look up definitions on https://dev.twitter.com/overview/api/users
 
 r_users_score <- r_users_info %>%
-  select(screen_name, followers_count, friends_count, statuses_count, listed_count, favourites_count) %>% 
+  filter(protected == FALSE) %>% 
+  select(screen_name, dplyr::contains("count")) %>% 
   unique() %>% 
-  mutate(followers_centile = ecdf(followers_count)(followers_count),
-         friends_centile = ecdf(friends_count)(friends_count),
-         listed_centile = ecdf(listed_count)(listed_count),
-         favourites_centile = ecdf(favourites_count)(favourites_count),
-         statuses_centile = ecdf(statuses_count)(statuses_count)
+  mutate(followers_percentile = ecdf(followers_count)(followers_count),
+         friends_percentile = ecdf(friends_count)(friends_count),
+         listed_percentile = ecdf(listed_count)(listed_count),
+         favourites_percentile = ecdf(favourites_count)(favourites_count),
+         statuses_percentile = ecdf(statuses_count)(statuses_count)
          ) %>% 
   arrange(screen_name) 
 
@@ -119,10 +124,11 @@ head(r_users_score)
 tail(r_users_score)
 str(r_users_score)
 
+### it does not say how many of those tweets were actual #rstats tweets!
 
 r_users_ranking = r_users_score %>% 
   group_by(screen_name) %>% 
-  summarise(top_score = followers_centile + friends_centile + listed_centile + favourites_centile + statuses_centile)
+  summarise(top_score = followers_percentile + friends_percentile + listed_percentile + favourites_percentile + statuses_percentile)
 
 top_10 <- r_users_ranking %>% arrange(desc(top_score)) %>% head(10)
 top_100 <- r_users_ranking %>% arrange(desc(top_score)) %>% head(100)
@@ -142,47 +148,224 @@ top10_ids <- r_users_info %>%
 id <- top10_ids$user_id
 
 
+#### get followers 
+friends1 <- get_friends("hspter")
+friends_test <- map(top_10$screen_name, get_friends)
+
+top10_friends_info1<-lookup_users(friends_test[1])
 
 
 
-#### twitteR hack to get followers of multiple users #####
-
-### single example ###
-
-# friends object
-start <- getUser("@camharvey")
-friends_object <- lookupUsers(start$getFriendIDs())
-friendsCount(start)
-
-# followers object 
-followers_object <- lookupUsers(start$getFollowerIDs())
-followers_object
-followersCount(start)
+followers_test1 <- get_followers(top_10$screen_name[1])
+followers_test2 <- get_followers(top_10$screen_name[2]) 
+followers_test3 <- get_followers(top_10$screen_name[3]) 
+followers_test4 <- get_followers(top_10$screen_name[4]) 
+followers_test5 <- get_followers(top_10$screen_name[5]) 
+followers_test6 <- get_followers(top_10$screen_name[6]) 
+followers_test7 <- get_followers(top_10$screen_name[7]) 
+followers_test8 <- get_followers(top_10$screen_name[8]) 
+followers_test9 <- get_followers(top_10$screen_name[9]) 
+followers_test10 <- get_followers(top_10$screen_name[10]) 
 
 
-friends <- sapply(friends_object[1:117],name)
-followers <- sapply(followers_object[1:1033],name)
+followers_test1B <- data.frame(r_user = rep(top_10$screen_name[1], length(followers_test1)), follower_id = followers_test1)
+followers_test2B <- data.frame(r_user = rep(top_10$screen_name[2], length(followers_test2)), follower_id = followers_test2)
+followers_test3B <- data.frame(r_user = rep(top_10$screen_name[3], length(followers_test3)), follower_id = followers_test3)
+followers_test4B <- data.frame(r_user = rep(top_10$screen_name[4], length(followers_test4)), follower_id = followers_test4)
+followers_test5B <- data.frame(r_user = rep(top_10$screen_name[5], length(followers_test5)), follower_id = followers_test5)
+followers_test6B <- data.frame(r_user = rep(top_10$screen_name[6], length(followers_test6)), follower_id = followers_test6)
+followers_test7B <- data.frame(r_user = rep(top_10$screen_name[7], length(followers_test7)), follower_id = followers_test7)
+followers_test8B <- data.frame(r_user = rep(top_10$screen_name[8], length(followers_test8)), follower_id = followers_test8)
+followers_test9B <- data.frame(r_user = rep(top_10$screen_name[9], length(followers_test9)), follower_id = followers_test9)
+followers_test10B <- data.frame(r_user = rep(top_10$screen_name[10], length(followers_test10)), follower_id = followers_test10)
 
-# Merge both lists into a data frame to create an edge file from followers and friends
+final_followers <- data.frame(rbind(followers_test1B, followers_test2B, followers_test3B, followers_test4B, followers_test5B,
+                                    followers_test6B, followers_test7B, followers_test8B, followers_test9B, followers_test10B))
+  
+str(final_followers)
 
-relations <- merge(data.frame(User='@camharvey',followers=friends), data.frame(User=followers, followers='@camharvey'), all=TRUE)
-a <- data.frame(User='@camharvey',followers=friends)
-str(a)
-head(a)
+list_test <- list(followers_test1, followers_test2, followers_test3, followers_test4, followers_test5,
+                  followers_test6, followers_test7, followers_test8, followers_test9, followers_test10)
+str(final_followers)
+summary(final_followers)
 
-b <- data.frame(User=followers, followers='@camharvey')
-str(b)
-head(b)
+head(final_followers)
+tail(final_followers)
+
+d1 <- graph_from_data_frame(final_followers, directed = TRUE, vertices = NULL)
+
+
+g1 <- create.igraph(data)
+mdslayout <- layout.mds(g1, dist = distance_of_your_nodes)
+plot.igraph(g1, layout = mdslayout)
+
+
+graph<-graph_from_data_frame(dummy_data2[1:100, ])
+
+
+ggraph(d1) + 
+  geom_edge_link() + 
+  geom_node_point()
+
+
+geom_edge_link(aes(colour = factor(season)))+
+  geom_node_point(aes(size=ifelse(V(graph)$name %in% n.names,1,degree)),
+                  colour=ifelse(V(graph)$name %in% n.names,'#363636','#ffffff'),
+                  show.legend = F)
 
 
 
 
-library(twitteR)
+
+str(list_test_names)
+
+  set_names(map
+            _chr(top_10, "screen_name"))
+
+obama2 <- get_friends("BarackObama", page = next_cursor(obama1))
+
+
+
+
+colbert_nation <- get_followers("stephenathome", n = 18000)
+
+get_followers(top_10$screen_name[1], n = 18000)
+
+
+follower_test <- map(top_10$screen_name, get_followers, n = 15000)
+
+
+n_test1 <- get_followers(top_10$screen_name[1], n = 5000)
+n_test2 <- get_followers(top_10$screen_name[2], n = 18000)
+n_test3 <- get_followers(top_10$screen_name[3], n = 18000)
+n_test4 <- get_followers(top_10$screen_name[4], n = 18000)
+n_test5 <- get_followers(top_10$screen_name[5], n = 18000)
+n_test6 <- get_followers(top_10$screen_name[6], n = 18000)
+n_test7 <- get_followers(top_10$screen_name[7], n = 18000)
+n_test8 <- get_followers(top_10$screen_name[8], n = 18000)
+n_test9 <- get_followers(top_10$screen_name[9], n = 18000)
+n_test10 <- get_followers(top_10$screen_name[10], n = 18000)
+
+
+
+
+
+
+
+
+
+
 
 ?getUser
 
-users <- top10_ids$user_id #List of usernames
+str(top_10)
+top_10$screen_name
+
+users <- top10_ids$user_id #List of user ids
+users_names <- top10_ids$screen_name #List of usernames
 userrelations <- list() #Create an empty list to populate
+
+
+#### dummy network ####
+
+?expand.grid
+
+users_names
+str(users_names)
+dummy_data <- expand.grid(from = users_names, to = users_names) %>% as.data.frame()
+dummy_data$status <- sample(c("follower", "friend"), 100, replace =  TRUE) %>% as.data.frame()
+dummy_data2 <- as.data.frame(user_el)
+str(dummy_data)  
+
+
+update.packages(checkBuilt = TRUE)
+install.packages("ggplot2")
+
+library(ggplot2)
+library(ggraph)
+library(igraph)
+library(dplyr)
+
+graph<-graph_from_data_frame(dummy_data2[1:100, ])
+
+ggraph(graph) + 
+  geom_edge_link() + 
+  geom_node_point()
+  
+  
+  geom_edge_link(aes(colour = factor(season)))+
+  geom_node_point(aes(size=ifelse(V(graph)$name %in% n.names,1,degree)),
+                  colour=ifelse(V(graph)$name %in% n.names,'#363636','#ffffff'),
+                  show.legend = F)
+
+devtools::install_github("briatte/ggnet")
+library(ggnet)
+
+
+installed.packages("network")
+installed.packages("sna")
+#remove.packages("scales")
+install.packages("scales")
+library(scales)
+library(network)
+library(sna)
+library(ggplot2)
+
+net = network(dummy_data, directed = TRUE)
+
+
+
+
+
+
+
+library(purrr)
+library(twitteR)
+
+
+str(top_10)
+
+
+?userTimeline
+top10_tweets <- map(top_10$screen_name, userTimeline,100)
+
+
+# EXTRACT each tweet text for screen name and count number of those containing #rstats hash
+head(top10_tweets)
+
+test2 <- list(top10_tweets[[2]])
+str(test2)
+test2B <-  as.data.frame(test2)
+
+test2[[1]][[2]]$created
+test2[[1]][[2]]$screenName
+test2[[1]][[2]]$text
+
+
+
+str(test2[[2]])
+top10_tweets[[1]][[2]]
+
+test_all <- as.data.frame(matrix(unlist(top10_tweets))) 
+str(test_all)
+head(test_all)
+
+
+start <- getUser(users[1])
+friends_object <- lookupUsers(start$getFriendIDs())
+followers_object <- lookupUsers(start$getFollowerIDs())
+
+friends <- sapply(1:length(friends_object), 
+                  function(x) name(friends_object[[x]]))
+followers <- sapply(1:length(followers_object), 
+                    function(x) name(followers_object[[x]]))
+userrelations[[i]] <- merge(data.frame(User=users[i],followers=friends),
+                            data.frame(User=followers, followers=users[i]), 
+                            all=TRUE)
+
+load("/Users/katarzynakulma/projects/animated_maps/twitter_rstats.RData")
+
+
 
 for (i in 1:length(users)){
   start <- getUser(users[i])
@@ -227,12 +410,55 @@ str(top_100_users)
 top_10_users <- top_10$screen_name
 str(top_10_users)
 
+
+
+
+##### STUFF I TRIED ####
+
+
+
+
+
+#### twitteR hack to get followers of multiple users #####
+
+### single example ###
+
+# friends object
+start <- getUser("@camharvey")
+friends_object <- lookupUsers(start$getFriendIDs())
+friendsCount(start)
+
+# followers object 
+followers_object <- lookupUsers(start$getFollowerIDs())
+followers_object
+followersCount(start)
+
+
+friends <- sapply(friends_object[1:117],name)
+followers <- sapply(followers_object[1:1033],name)
+
+# Merge both lists into a data frame to create an edge file from followers and friends
+
+relations <- merge(data.frame(User='@camharvey',followers=friends), data.frame(User=followers, followers='@camharvey'), all=TRUE)
+a <- data.frame(User='@camharvey',followers=friends)
+str(a)
+head(a)
+
+b <- data.frame(User=followers, followers='@camharvey')
+str(b)
+head(b)
+
+
+
+
+
 ### getting followers -> write a function!!
 
 
 test_followers <- get_followers("drob", n = 18000)
 str(test_followers)
 
+top_10_users
 
 library(purrr)
 token<-create_token(app="R-play",api_key,api_secret) 
